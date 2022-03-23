6b34a80cd57e69a98ee2c17b24c00a0675a6bdc5
kiri3914 <you@example.com>
2022-03-23 19:05:19 +0600

    create heroku settings

--- index.py
+++ index.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/name={name}&message={message}/')
-def get_hello(name: str , message:str):
+def get_hello(name: str , message: str):
     return f"Hello {name}! {message}!"
